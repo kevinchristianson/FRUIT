5c1df76b5c1f9be192593c8d2553c4e5ddbf320f
Anton Nagy <nagya@carleton.edu>
2019-06-07 17:04:47 -0500

    Updated initializing code and movie and user data dictionary text files

--- thePythonFileWhereAIHappens.py
+++ thePythonFileWhereAIHappens.py
@@ -1,50 +1,48 @@
-import pandas
-import json
-
-# class MovieNode():
-#     def __init__(self, movieID):
-#         self.movieID = movieID
-#         self.users = {}
-#
-# class UserNode():
-#     def __init__(self, userID):
-#         self.userID = userID
-#         self.movies = {}
+from pandas import read_csv
+from json import dump, load
+from pathlib import Path
 
 movieDict = {}
 userDict = {}
 
 def initialize():
-    data = pandas.read_csv("./data/ml-20m/ratings.csv")
-    for i in range(data.count()["userId"]):
-        userId = data["userId"][i]
-        movieId = data["movieId"][i]
-        rating = float(data["rating"][i])
-        if userId not in userDict:
-            # userNode = UserNode(userId)
-            userDict[str(userId)] = {}
-        if movieId not in movieDict:
-            # movieNode = MovieNode(movieId)
-            movieDict[str(movieId)] = {}
-        if rating <= 2:
-            rating = -1
-        elif rating >= 4:
-            rating = 1
-        else:
-            rating = 0
-        movieDict[str(movieId)][str(userId)] = rating
-        userDict[str(userId)][str(movieId)] = rating
+    MOVIE_FILE = "data/movie_dict.txt"
+    USER_FILE = "data/user_dict.txt"
 
-    file = open("./data/movie_dict.txt", "w")
-    json.dump(movieDict, file, ensure_ascii=False)
-    file.close()
-    file = open("./data/user_dict.txt", "w")
-    json.dump(userDict, file, ensure_ascii=False)
-    file.close()
+    global movieDict
+    global userDict
 
+    if Path(MOVIE_FILE).is_file() and Path(USER_FILE).is_file():
+        movieDict = load(open(MOVIE_FILE))
+        userDict = load(open(USER_FILE))
+        for key in movieDict:
+            if len(movieDict[key]) > 1:
+                break
+    else:
+        data = read_csv("data/ml-20m/ratings.csv")
+        for i in range(data.count()["userId"]):
+            userId = str(data["userId"][i])
+            movieId = str(data["movieId"][i])
+            rating = float(data["rating"][i])
+            if userId not in userDict:
+                # userNode = UserNode(userId)
+                userDict[userId] = {}
+            if movieId not in movieDict:
+                # movieNode = MovieNode(movieId)
+                movieDict[movieId] = {}
+            if rating <= 2:
+                rating = -1
+            elif rating >= 4:
+                rating = 1
+            else:
+                rating = 0
+            movieDict[movieId][userId] = rating
+            userDict[userId][movieId] = rating
 
-
-
+        with open(MOVIE_FILE, "w") as file:
+            dump(movieDict, file, ensure_ascii=False)
+        with open(USER_FILE, "w") as file:
+            dump(userDict, file, ensure_ascii=False)
 
 def main():
     initialize()
